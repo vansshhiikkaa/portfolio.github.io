92bd94a89d84e419061c765f9955b35bf5767515
vansshhiikkaa <105902017+vansshhiikkaa@users.noreply.github.com>
2024-11-15 16:39:15 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
     backSpeed: 20,
     smartBackspace: false,
     loop: true,
-    showCursor: false,
+    showCursor: true,
   });
 });
 
